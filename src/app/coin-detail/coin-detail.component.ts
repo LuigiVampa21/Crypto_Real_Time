fbbb7c1579610fe8c35b8e5c246677246946eee6
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-10-14 03:52:42 +0200

    ntn

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Coin } from '../models/coin.model';
 import { CoinService } from '../services/coin.service';
 import { Subscription } from 'rxjs';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 
 @Component({
@@ -11,11 +13,45 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./coin-detail.component.scss']
 })
 export class CoinDetailComponent implements OnInit, OnDestroy {
-
+  
+  @ViewChild(BaseChartDirective) myLineChart !: BaseChartDirective;
   currency = 'USD'
   id!:string;
   coin!:Coin;
   coinSub = new Subscription();
+  days = 1;
+
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: `Price Trends`,
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#009688',
+        pointBackgroundColor: '#009688',
+        pointBorderColor: '#009688',
+        pointHoverBackgroundColor: '#009688',
+        pointHoverBorderColor: '#009688',
+
+      }
+    ],
+    labels: []
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      point: {
+        radius: 1
+      }
+    },
+
+    plugins: {
+      legend: { display: true },
+    }
+  };
+  public lineChartType: ChartType = 'line';
+
+  
   constructor(private coinService: CoinService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -25,10 +61,28 @@ export class CoinDetailComponent implements OnInit, OnDestroy {
       this.coinSub =  this.coinService.getSingleCoin(this.id)
           .subscribe(coin => {
             this.coin = coin;
-            console.log(this.coin);
-            
-          })
-        
+            // console.log(this.coin);
+          }),
+          this.coinService.getCoinGraph(this.days, this.id)
+            .subscribe( graph => {
+              this.lineChartData.datasets[0].data = graph.prices.map(
+                  (a:[time:number,price:number][]) => {
+                      return a[1]
+                  });
+              this.lineChartData.labels = graph.prices.map(
+                  // (a:[time:number,price:number][]) => {
+                  (a:any) => {
+                    let date = new Date(a[0]);
+                    // let time = `${date.getHours()}: ${date.getMinutes()}`
+                    let time = date.getHours()
+                                 > 12 ?
+        `${date.getHours() - 12}: ${date.getMinutes()} PM` :
+        `${date.getHours()}: ${date.getMinutes()} AM`
+       
+        return this.days === 1 ? time : date.toLocaleDateString(); 
+                  })
+            }
+            )        
       })
     
   }
